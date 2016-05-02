9460f345b573b9f6825669692f100fa91f07b8d7
Udyatbhanu <udyatbhanu@gmail.com>
2016-05-02 00:41:29 -0700

    Initial commit

---
+++ theme-lib/src/main/java/com/home/ubbs/lib/theme/BaseThemeActivity.java
@@ -0,0 +1,54 @@
+package com.home.ubbs.lib.theme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.StyleRes;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.StyleRes;
+import android.support.v7.app.AppCompatActivity;
+
+import com.home.ubbs.lib.theme.ThemeSettings;
+
+/**
+ * Created by udyatbhanu-mac on 4/30/16.
+ */
+public class BaseThemeActivity extends AppCompatActivity {
+
+    protected static final String THEME_KEY = "theme";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //setup Theme
+        setUpTheme(ThemeSettings.style);
+        super.onCreate(savedInstanceState);
+    }
+
+
+    /**
+     *
+     * @param style
+     */
+    private void setUpTheme(final @StyleRes int style){
+        if(style!=-1){
+            setTheme(style);
+        }
+
+    }
+
+    /**
+     *
+     * @param theme
+     */
+    protected void setTheme(final @StyleRes int style, BaseThemeActivity activity){
+        ThemeSettings.style = style;
+        Intent intent = new Intent(getBaseContext(),activity.getClass());
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ theme-lib/src/main/java/com/home/ubbs/lib/theme/ThemeSettings.java
@@ -0,0 +1,26 @@
+package com.home.ubbs.lib.theme;
+
+import android.support.annotation.StyleRes;
+
+/**
+ * Created by udyatbhanu-mac on 4/30/16.
+ */
+public class ThemeSettings {
+
+    public  static @StyleRes
+    int style = -1;
+
+    private ThemeSettings(){
+
+    }
+
+    public static int getStyle() {
+        return style;
+    }
+
+
+
+
+
+
+}
